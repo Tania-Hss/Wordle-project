df9731bca0d52f34c1b05895e1f8d1c48ad25e0f
Tania Hosseini <tania.hosseinii@gmail.com>
2023-01-21 13:10:28 +1100

    stage 1 keyboard

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const tiles = document.querySelector('.gameboard')
 const keyboard = document.quarySelector('.keyboard')
+const words = []
+const keys = ['Q', 'W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','ENTER','Z','X','C','V','B','N','M','⌫',]
 
-const keys = ['Q', 'W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
