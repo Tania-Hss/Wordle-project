ee82015eacb1688892a44ce1f1bd84eef3306454
Tania Hosseini <tania.hosseinii@gmail.com>
2023-01-23 20:11:25 +1100

    delete button & displaying letters in tiles

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-
 const tileDisplay = document.querySelector('.gameboard');
+let currentTile = 1
 
-function createTileDisplay() {
+function createTiles() {
     for(i = 0; i < 30; i++) {
     const tiles = document.createElement('div')
     tiles.classList.add('tile')
     tiles.setAttribute('id', i + 1)
     tileDisplay.appendChild(tiles);
+    }
 }
+createTiles()
+
+const keys = document.querySelectorAll('.row button');
+for(i = 0; i < keys.length; i++) {
+    keys[i].onclick = ({target}) => {
+        const key = target.getAttribute('data-set');
+        addLetterToTiles(key)
+    };
 }
-createTileDisplay()
+
+
+function addLetterToTiles(letter) {
+    if (currentTile <= 30) {
+        const tile = document.getElementById(currentTile)
+        tile.textContent = letter
+        currentTile = currentTile + 1
+    } else {
+        console.log('nahhhh enough ')
+    }
+}
+
+function myFunction () {
+    document.getElementById("enter").click(console.log('yayyyyy'));
+}
+
+
+
+
+
