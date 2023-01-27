36d5d726c3cc283a9a31e78f632a2847717d21ea
Tania Hosseini <tania.hosseinii@gmail.com>
2023-01-28 00:37:44 +1100

    settimeout and animation working

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ _hints_
 - grey tile: letter is not in word 
 - green tile: letter is in the word and in the correct position
 - yellow tile: letter is in the word but in the wrong position
-(using data attribute)
+
 ## game structure
 heading
 board 5*6
@@ -22,25 +22,33 @@ need an array of words
 ## animations
 
 
-[flipInX animations](https://animate.style/)
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>     
+[Animation from](https://animate.style/)
+
 ## functionality
 
-- function for creating the game board 
-- creating the keyboard
-backspacing + entering 
-- typing letter should give an output in the tiles (move to the next line ++)
-- backspace will delete letters from the tile
-- enter key will submit the letter and check for answer
+- function for creating the board that has 5 colums and 6 rows
+- creating the keyboard in html and using a for loop that goes through each key and listens for button being clicked.
+- function for letter displays in the tiles (move to the next line ++) when player presses keyboard buttons
+- selecting the enter buton and listening for button being clicked to perform a function
+- creating an empty array that saves each letter the player clicks on 
+- function that .joins player letters as a single string (word)
+- function that sets winning conditions using if(if else) statement and depending on conditions returns a color.
+- settimeout method inside function that takes playerletters and uses for each loop that takes Eachletter and sets the background color to winning condition function after waiting for seconds.
+- backspacing (nice to have/not essential)
+- animation 
 
 
 ## message
 
 alerts
-- when word not in word list
+
 - when they correctly guess the word
 - when they have 0 number of guesses left (lose)
 - 
 
 
 [Original Wordle](https://www.nytimes.com/games/wordle/index.html)
+[settimeout method](https://www.w3schools.com/jsref/met_win_settimeout.asp)
+[array methods](https://www.w3schools.com/js/js_array_methods.asp#:~:text=JavaScript%20Array%20Methods%201%20Converting%20Arrays%20to%20Strings,%28%29%20...%208%20Changing%20Elements%20...%20More%20items)
+[COLOR RGBs](https://www.rapidtables.com/web/color/RGB_Color.html#:~:text=Basic%20colors%3A%20%20%20%20HTML%20%2F%20CSS,%20%20%280%2C255%2C0%29%20%2012%20more%20rows%20)
+[AnimationFlipInX](https://animate.style/)
--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const tileDisplay = document.querySelector('.gameboard');
-let currentTile = 1;
-let winningWord = 'START';
+let winningWord = 'LOWER';
 let row = 0;
-
+let currentTile = 1;
 // Create a global variable that saves each letter the player enters.
 let playerLetters = []
 
 
+const tileDisplay = document.querySelector('.gameboard');
 function createTiles() {
     for(i = 0; i < 30; i++) {
         const tiles = document.createElement('div')
         tiles.classList.add('tile')
+        tiles.classList.add('animate__animated')
         tiles.setAttribute('id', i + 1)
         tileDisplay.appendChild(tiles)
     }
@@ -20,42 +20,43 @@ createTiles()
 
 
 const keys = document.querySelectorAll('.row button');
+
 for(i = 0; i < keys.length; i++) {
     keys[i].onclick = ({target}) => {
-        const key = target.getAttribute('data-set');
-        addLetterToTiles(key)
-        console.log('clicked ' + key)
-        playerLetters.push(key)
+        const letter = target.getAttribute('data-set');
+        addLetterToTiles(letter)
+        console.log('clicked ' + letter)
+        playerLetters.push(letter)
     };
     
 }
 
 
-
 function addLetterToTiles(letter) {
-    
     if (currentTile <= 30) {
         const tile = document.getElementById(currentTile)
-        tile.textContent = letter
+        tile.textContent = letter 
         currentTile = currentTile + 1
-    } else {
+    } 
+    else {
         window.alert(`Bad Luck! The Word Is ${winningWord}`)
     }
     
 }
 
-const backspacebtn = document.querySelector('#backspace')
-backspacebtn.addEventListener('click', () => {
+const removeButton = document.querySelector('#backspace')
+removeButton.addEventListener('click', () => {
     console.log('clicked backspace')
-    // .remove() or .pop()
+    
+    tile.textContent = ''
     
 })
 
 const enterbtn = document.querySelector('#enter')
 enterbtn.addEventListener('click', () => {
-    console.log('clicked Enter')
     console.log(playerLetters)
     newGuess()
+
 })
 
 function newGuess() {
@@ -64,17 +65,20 @@ function newGuess() {
     if (guess === winningWord){
         window.alert('You Win!')
     }
+    
     //if row = 0 first letter in row will be 1 
     //if row = 1 first letter in row will be 6 which is first letter of row 1  / next guess
     playerLetters.forEach((letter, i) => {
-    const firstLetterInRow = row * 5 + 1;
+        const firstLetterInRow = row * 5 + 1;
         setTimeout(() => {
             const letterIndex = firstLetterInRow + i;
             const eachLetter = document.getElementById(letterIndex)
-            eachLetter.style = `background-color:${winningConditions(letter, i)}`
+            eachLetter.style.backgroundColor = winningConditions(letter, i)
+            eachLetter.classList.add('animate__flipInX')
         }, 500);
-        
+    
     });
+    row = row + 1
     playerLetters = []
 }
 
@@ -92,22 +96,3 @@ function winningConditions(letter, i) {
     } 
 }
 
-/*
-to compare the winning word to player word by the letter and the position of letter
-making tiles change color to yellow green grey depending if their word matches the winning word
-if winningword === letterposition
-if winningword === letter
-
-
-
-right letter right position = green
-right letter wrong position = yellow
-wrong letter wrong position = grey
-
-bugs
-
-if player guesses word correctly stop the game 
-
-
-if player guesses word dont run the 'you lose' message anymore
-*/
